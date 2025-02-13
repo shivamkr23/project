9429dd0b1d14d08a98381d5e1dd84a0ed721fac8
Shivam Kumar <shivamkr.kr23@gmail.com>
2025-02-13 15:09:30 +0000

    added correct api paths

--- hello.py
+++ hello.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from project!")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+app.add_middleware(CORSMiddleware, allow_headers=["*"], allow_origins=["*"])
+
+@app.post("/run")
+def post_run(task:str):
+    return {"task": task}
+
+@app.get("/read")
+def get_read(path:str):
+    return {"path": path}
 
 
 if __name__ == "__main__":
-    main()
+    import uvicorn
+    uvicorn.run("hello:app", host="127.0.0.1", port=8000)
